64b398a66fcd6330f2a2a8a61e8263f585d6da61
Juan Camilo Rojas <juancamilor576@gmail.com>
2025-05-04 18:27:58 -0500

    feat: logo expo project mobile

--- src/components/section/projects/projects.tsx
+++ src/components/section/projects/projects.tsx
@@ -1,4 +1,3 @@
-
 import TypographyH2 from "@/components/shared/typography-h2";
 import TypographyP from "@/components/shared/typography-p";
 import CarouselItems from "@/components/shared/carousel-items";
@@ -18,164 +17,199 @@ import ElectronLogo from "@/components/icons/electron-logo";
 import ShadcnLogo from "@/components/icons/shadcn-log";
 
 const Projects = () => {
-  const project1 = dataProjects[0];
-  const project2 = dataProjects[1];
-  const project3 = dataProjects[2];
-  const project4 = dataProjects[3];
-  const project5 = dataProjects[4];
-  return (
-    <>
-      <Project state={project1.state} github={project1.github} url={project1.url}>
-        <div className="max-sm:px-0 py-2 w-full">
-          <div className="mb-4 w-full">
-            <TypographyH2 text={project1.title} className="pb-2 font-extrabold text-blue-400 text-2xl" />
+	const project1 = dataProjects[0];
+	const project2 = dataProjects[1];
+	const project3 = dataProjects[2];
+	const project4 = dataProjects[3];
+	const project5 = dataProjects[4];
+	return (
+		<>
+			<Project
+				state={project1.state}
+				github={project1.github}
+				url={project1.url}
+			>
+				<div className="max-sm:px-0 py-2 w-full">
+					<div className="mb-4 w-full">
+						<TypographyH2
+							text={project1.title}
+							className="pb-2 font-extrabold text-blue-400 text-2xl"
+						/>
 
-            <TypographyP className="leading-6">
-              {project1.description}
-            </TypographyP>
+						<TypographyP className="leading-6">
+							{project1.description}
+						</TypographyP>
 
-            <ContainerFlex className="justify-start mt-4 w-full h-8">
-              <TooltipItem text="React">
-                <ReactLogo />
-              </TooltipItem>
-              <TooltipItem text="Shadcn">
-                <ShadcnLogo />
-              </TooltipItem>
-              <TooltipItem text="Tailwind">
-                <TailwindLogo />
-              </TooltipItem>
-              <TooltipItem text="Firebase">
-                <FirebaseLogo />
-              </TooltipItem>
-              <TooltipItem text="React Router Dom">
-                <ReactRouterLogo />
-              </TooltipItem>
-              <TooltipItem text="TypeScript">
-                <TypeScriptLogo />
-              </TooltipItem>
-            </ContainerFlex>
+						<ContainerFlex className="justify-start mt-4 w-full h-8">
+							<TooltipItem text="React">
+								<ReactLogo />
+							</TooltipItem>
+							<TooltipItem text="Shadcn">
+								<ShadcnLogo />
+							</TooltipItem>
+							<TooltipItem text="Tailwind">
+								<TailwindLogo />
+							</TooltipItem>
+							<TooltipItem text="Firebase">
+								<FirebaseLogo />
+							</TooltipItem>
+							<TooltipItem text="React Router Dom">
+								<ReactRouterLogo />
+							</TooltipItem>
+							<TooltipItem text="TypeScript">
+								<TypeScriptLogo />
+							</TooltipItem>
+						</ContainerFlex>
+					</div>
+					<div className="justify-center items-center w-full h-[300px]">
+						<CarouselItems items={project1.images} />
+					</div>
+				</div>
+			</Project>
 
-          </div>
-          <div className="justify-center items-center w-full h-[300px]">
-            <CarouselItems items={project1.images} />
-          </div>
-        </div>
-      </Project>
+			<Project
+				state={project2.state}
+				github={project2.github}
+				url={project2.url}
+			>
+				<div className="max-sm:px-0 py-2 w-full">
+					<div className="mb-4 w-full">
+						<TypographyH2
+							text={project2.title}
+							className="pb-2 font-extrabold text-blue-400 text-2xl"
+						/>
 
-      <Project state={project2.state} github={project2.github} url={project2.url}>
-        <div className="max-sm:px-0 py-2 w-full">
-          <div className="mb-4 w-full">
-            <TypographyH2 text={project2.title} className="pb-2 font-extrabold text-blue-400 text-2xl" />
+						<TypographyP className="leading-6">
+							{project2.description}
+						</TypographyP>
 
-            <TypographyP className="leading-6">
-              {project2.description}
-            </TypographyP>
+						<ContainerFlex className="justify-start mt-4 w-full h-8">
+							<TooltipItem text="Electron">
+								<ElectronLogo />
+							</TooltipItem>
+							<TooltipItem text="React">
+								<ReactLogo />
+							</TooltipItem>
+							<TooltipItem text="Shadcn">
+								<ShadcnLogo />
+							</TooltipItem>
+							<TooltipItem text="Tailwind">
+								<TailwindLogo />
+							</TooltipItem>
+							<TooltipItem text="TypeScript">
+								<TypeScriptLogo />
+							</TooltipItem>
+						</ContainerFlex>
+					</div>
+					<div className="justify-center items-center w-full h-[300px]">
+						<CarouselItems items={project2.images} />
+					</div>
+				</div>
+			</Project>
 
-            <ContainerFlex className="justify-start mt-4 w-full h-8">
-              <TooltipItem text="Electron">
-                <ElectronLogo />
-              </TooltipItem>
-              <TooltipItem text="React">
-                <ReactLogo />
-              </TooltipItem>
-              <TooltipItem text="Shadcn">
-                <ShadcnLogo />
-              </TooltipItem>
-              <TooltipItem text="Tailwind">
-                <TailwindLogo />
-              </TooltipItem>
-              <TooltipItem text="TypeScript">
-                <TypeScriptLogo />
-              </TooltipItem>
-            </ContainerFlex>
+			<Project
+				state={project3.state}
+				github={project3.github}
+				url={project3.url}
+			>
+				<div className="max-sm:px-0 py-2 w-full">
+					<div className="mb-4 w-full">
+						<TypographyH2
+							text={project3.title}
+							className="pb-2 font-extrabold text-blue-400 text-2xl"
+						/>
 
-          </div>
-          <div className="justify-center items-center w-full h-[300px]">
-            <CarouselItems items={project2.images} />
-          </div>
-        </div>
-      </Project>
+						<TypographyP className="leading-6">
+							{project3.description}
+						</TypographyP>
 
-      <Project state={project3.state} github={project3.github} url={project3.url}>
-        <div className="max-sm:px-0 py-2 w-full">
-          <div className="mb-4 w-full">
-            <TypographyH2 text={project3.title} className="pb-2 font-extrabold text-blue-400 text-2xl" />
+						<ContainerFlex className="justify-start mt-4 w-full h-8">
+							<div className="h-7 iconsLinks">
+								<ReactLogo />
+							</div>
+							<div className="h-7 iconsLinks">
+								<TailwindLogo />
+							</div>
+							<div className="h-7 iconsLinks">
+								<FirebaseLogo />
+							</div>
+						</ContainerFlex>
+					</div>
+					<div className="justify-center items-center w-full h-[300px]">
+						<CarouselItems items={project3.images} />
+					</div>
+				</div>
+			</Project>
 
-            <TypographyP className="leading-6">
-              {project3.description}
-            </TypographyP>
+			<Project
+				state={project4.state}
+				github={project4.github}
+				url={project4.url}
+			>
+				<div className="max-sm:px-0 py-2 w-full">
+					<div className="mb-4 w-full">
+						<TypographyH2
+							text={project4.title}
+							className="pb-2 font-extrabold text-blue-400 text-2xl"
+						/>
 
-            <ContainerFlex className="justify-start mt-4 w-full h-8">
-              <div className="h-7 iconsLinks">
-                <ReactLogo />
-              </div>
-              <div className="h-7 iconsLinks">
-                <TailwindLogo />
-              </div>
-              <div className="h-7 iconsLinks">
-                <FirebaseLogo />
-              </div>
-            </ContainerFlex>
+						<TypographyP className="leading-6">
+							{project4.description}
+						</TypographyP>
 
-          </div>
-          <div className="justify-center items-center w-full h-[300px]">
-            <CarouselItems items={project3.images} />
-          </div>
-        </div>
-      </Project>
+						<ContainerFlex className="justify-start mt-4 w-full h-8">
+							<div className="h-7 iconsLinks">
+								<ReactLogo />
+							</div>
+							<div className="h-7 iconsLinks">
+								<TailwindLogo />
+							</div>
+							<div className="h-7 iconsLinks">
+								<ExpoLogo />
+							</div>
+						</ContainerFlex>
+					</div>
+					<div className="justify-center items-center w-full h-[300px]">
+						<CarouselItems items={project4.images} />
+					</div>
+				</div>
+			</Project>
 
-      <Project state={project4.state} github={project4.github} url={project4.url}>
-        <div className="max-sm:px-0 py-2 w-full">
-          <div className="mb-4 w-full">
-            <TypographyH2 text={project4.title} className="pb-2 font-extrabold text-blue-400 text-2xl" />
+			<Project
+				state={project5.state}
+				github={project5.github}
+				url={project5.url}
+			>
+				<div className="max-sm:px-0 py-2 w-full">
+					<div className="mb-4 w-full">
+						<TypographyH2
+							text={project5.title}
+							className="pb-2 font-extrabold text-blue-400 text-2xl"
+						/>
 
-            <TypographyP className="leading-6">
-              {project4.description}
-            </TypographyP>
+						<TypographyP className="leading-6">
+							{project5.description}
+						</TypographyP>
 
-            <ContainerFlex className="justify-start mt-4 w-full h-8">
-              <div className="h-7 iconsLinks">
-                <ReactLogo />
-              </div>
-              <div className="h-7 iconsLinks">
-                <TailwindLogo />
-              </div>
-            </ContainerFlex>
-
-          </div>
-          <div className="justify-center items-center w-full h-[300px]">
-            <CarouselItems items={project4.images} />
-          </div>
-        </div>
-      </Project>
-
-      <Project state={project5.state} github={project5.github} url={project5.url}>
-        <div className="max-sm:px-0 py-2 w-full">
-          <div className="mb-4 w-full">
-            <TypographyH2 text={project5.title} className="pb-2 font-extrabold text-blue-400 text-2xl" />
-
-            <TypographyP className="leading-6">
-              {project5.description}
-            </TypographyP>
-
-            <ContainerFlex className="justify-start mt-4 w-full h-8">
-              <div className="h-7 iconsLinks">
-                <ReactLogo />
-              </div>
-              <div className="h-7 iconsLinks">
-                <TailwindLogo />
-              </div>
-            </ContainerFlex>
-
-          </div>
-          <div className="justify-center items-center w-full h-[300px]">
-            <CarouselItems items={project5.images} />
-          </div>
-        </div>
-      </Project>
-
-    </>
-  );
+						<ContainerFlex className="justify-start mt-4 w-full h-8">
+							<div className="h-7 iconsLinks">
+								<ReactLogo />
+							</div>
+							<div className="h-7 iconsLinks">
+								<TailwindLogo />
+							</div>
+							<div className="h-7 iconsLinks">
+								<ExpoLogo />
+							</div>
+						</ContainerFlex>
+					</div>
+					<div className="justify-center items-center w-full h-[300px]">
+						<CarouselItems items={project5.images} />
+					</div>
+				</div>
+			</Project>
+		</>
+	);
 };
 
 export default Projects;
